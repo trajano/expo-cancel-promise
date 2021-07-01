5ea8cc918e1fc5c9d6b1397278bd3dec5b617400
Archimedes Trajano <archimedes.trajano@devhaus.com>
2021-07-01 13:16:46 -0400

    X

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Pressable } from 'react-native';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+const Blah = () => {
+  const [ val, setVal ] = useState(0);
+  useEffect( () => { 
+    const x = setTimeout(() => { setVal(1) }, 1000)
+    return () => clearTimeout(x)
+  }, [])
+  return (
+    <Text style={styles.blah}>Val = {val}</Text>
+  )
+}
 export default function TabOneScreen() {
+  const [showBlah, setShowBlah] = useState(false);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Pressable onPress={()=>{setShowBlah(!showBlah)}}>
+        <Text style={styles.button}>Toggle</Text>
+      </Pressable>
+      {showBlah  && (<Blah />)}
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -29,4 +47,11 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  button: {
+    padding: 10,
+    borderWidth: 1,
+  },
+  blah: {
+    fontSize: 20
+  }
 });
