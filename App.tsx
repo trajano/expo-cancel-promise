41f55e5a6e072eb7306a7488db09da793e277819
Archimedes Trajano <developer@trajano.net>
2021-07-02 13:26:11 -0400

    Remove bluebird

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as Promise from 'bluebird';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-Promise.config({
-  cancellation: true
-})
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
